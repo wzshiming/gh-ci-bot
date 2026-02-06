953d7f8af87dd3609561a20b8954833aaa755527
Shiming Zhang <wzshiming@hotmail.com>
2026-02-06 19:10:10 +0800

    Replace 'gh repo clone' with 'git clone' and remove remote set-url command

--- plugins/cherry-pick/cherry-pick.plugin.sh
+++ plugins/cherry-pick/cherry-pick.plugin.sh
@@ -35,7 +35,7 @@ cherry_pick_branch="cherry-pick-${ISSUE_NUMBER}-to-${branch}"
 tmpdir="$(mktemp -d)"
 trap 'rm -rf ${tmpdir}' EXIT
 
-gh repo clone "${GH_REPOSITORY}" "${tmpdir}" -- -b "${branch}" || {
+git clone "https://github.com/${GH_REPOSITORY}.git" "${tmpdir}" --branch "${branch}" || {
     echo "[FAIL] Failed to clone the repository or branch \`${branch}\` does not exist."
     exit 1
 }
@@ -44,7 +44,6 @@ cd "${tmpdir}" || exit 1
 
 git config --global user.email github-actions[bot]@users.noreply.github.com
 git config --global user.name github-actions[bot]
-git remote set-url origin "https://github.com/${GH_REPOSITORY}.git"
 
 git checkout -b "${cherry_pick_branch}"
 git cherry-pick "${merge_commit}" -m 1 || {
