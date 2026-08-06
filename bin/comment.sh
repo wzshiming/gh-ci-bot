d993f4d05edf097de0f9d9767ab5ac11a884a723
Shiming Zhang <wzshiming@hotmail.com>
2026-08-06 11:41:44 +0800

    add execution details link to bot replies

--- bin/comment.sh
+++ bin/comment.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-gh "${ISSUE_KIND}" -R "${GH_REPOSITORY}" comment "${ISSUE_NUMBER}" --body "${1}"
+body="${1}"
+
+if [[ -n "${GITHUB_RUN_ID:-}" ]]; then
+    server_url="${GITHUB_SERVER_URL:-https://github.com}"
+    repository="${GITHUB_REPOSITORY:-${GH_REPOSITORY}}"
+    run_url="${server_url%/}/${repository}/actions/runs/${GITHUB_RUN_ID}"
+    body="${body}
+
+<details>
+<summary>Execution details</summary>
+
+[Open the execution log](${run_url})
+
+</details>"
+fi
+
+gh "${ISSUE_KIND}" -R "${GH_REPOSITORY}" comment "${ISSUE_NUMBER}" --body "${body}"
